36e582e080767c390e4b609ec159ab93202f02f9
Arlette Cruz <arlettecruz@hotmail.com>
2021-10-18 17:34:02 -0500

    Added gitignore file and changes to HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -2,4 +2,7 @@
 This is a first sample program to upload to GitHub for assignment 2
 """
 
-print ("Hello World")
+if __name__ == '__main__':
+    print("Hello World!!")
+    name = "Arlette"
+    print(f"My name is {name}")
